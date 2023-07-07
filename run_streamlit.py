9aa8d03e2825cb0960c3a7a7eb91df10c547aa30
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-07 15:57:30 +0600

    run_streamlit.py

---
+++ run_streamlit.py
@@ -0,0 +1,5 @@
+import os #package is useful for joining information from different files (os.path.join)
+
+# Define the Projects' main file on which you will do all the manipulations
+APP_ENTRY_POINT = 'main.py'
+
