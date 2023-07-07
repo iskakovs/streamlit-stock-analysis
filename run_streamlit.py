65b76e40ddf2fbab1720e90de61ef113620721c2
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-07 15:58:01 +0600

    run_streamlit.py

--- run_streamlit.py
+++ run_streamlit.py
@@ -3,3 +3,7 @@ import os #package is useful for joining information from different files (os.pa
 # Define the Projects' main file on which you will do all the manipulations
 APP_ENTRY_POINT = 'main.py'
 
+# Define 'dir_path' as a variable for compiling all the files in one 'main' file
+dir_path = os.path.dirname(__file__)
+path = os.path.join(dir_path, APP_ENTRY_POINT)
+os.system('streamlit run "{}"'.format(path))
