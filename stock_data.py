66bea9182767cfc1cfd24089324d713595307464
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-07 16:04:57 +0600

    stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -37,4 +37,16 @@ def calculate_indices(data):
     :return: data with indices
     :rtype: Dataframe
     """
+  # Calculation of the SMA (Simple Moving Average) index
+    data['SMA'] = data['Close'].rolling(window=20).mean()
     
+    # Calculation of the RSI index (Relative Strength Index)
+    delta = data['Close'].diff()
+    up = delta.clip(lower=0)
+    down = -1 * delta.clip(upper=0)
+    avg_gain = up.rolling(window=14).mean()
+    avg_loss = down.rolling(window=14).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    data['RSI'] = rsi
+    return data   
