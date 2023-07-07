4543ec2fecd8f67436555566be7e416c7f2dcde8
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-07 16:01:35 +0600

    Create stock_data.py

---
+++ stock_data.py
@@ -0,0 +1,4 @@
+import logging #package is responsible for collecting all the logs (see the file)
+import yfinance as yf #package is responsible for collecting stock real market data from Yahoo!Finance
+
+logger = logging.getLogger()
