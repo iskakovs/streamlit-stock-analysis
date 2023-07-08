8a549bd3ce14e65ceb0f343cfab76bae3bb12066
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-08 15:22:48 +0600

    view.py

--- view.py
+++ view.py
@@ -7,3 +7,16 @@ import db_sql #import 'db_sql' file
 import stock_data #import 'stock_data' file
 
 logger = logging.getLogger()
+
+# Define 'config_streamlit' function with parameter 'config'.
+# Usine strealit we create a visually appealing file to see in the browser with a sidebar
+def config_streamlit(config, db_path):
+    """
+    config streamlit
+    :param config: config file
+    :type config: dict
+    :param db_path: database path
+    :type db_path: string
+    :return: None
+    """
+  
