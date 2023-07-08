65d2436e36f0a3109083902cb064585206f03b60
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-08 15:55:45 +0600

    Update repository.py

--- repository.py
+++ repository.py
@@ -1,1 +1,5 @@
+import helpers_config #imports the 'helpers_logging.py' file to the 'repository.py' file
+import os #package is useful for joining information from different files (os.path.join)
 
+# Here we define two functions 'get_config' and 'get_get_database_path'
+# 'get_config' used to load the configuration information from the 'config.toml' file
