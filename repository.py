67da6f459c702e9a756d3ef510259ef7995e8904
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-08 15:56:34 +0600

    Update repository.py

--- repository.py
+++ repository.py
@@ -3,3 +3,12 @@ import os #package is useful for joining information from different files (os.pa
 
 # Here we define two functions 'get_config' and 'get_get_database_path'
 # 'get_config' used to load the configuration information from the 'config.toml' file
+
+def get_config(toml_file='config.toml'):
+    """
+    get config information from a toml file
+    :param toml_file: toml_file name with extension
+    :type toml_file: str
+    :return: config information
+    :rtype: dict
+    """
