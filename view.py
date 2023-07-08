c428d30de77f3ef968f73bf2ad639687c86988ed
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-08 15:25:11 +0600

    view.py

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
 import logging #package is responsible for collecting all the logs (see the file)
+
 import pandas as pd #import pandas
 import streamlit as st #import streamlit (used for creating a visual app in your browser)
+
 import db_sql #import 'db_sql' file
 import stock_data #import 'stock_data' file
 
