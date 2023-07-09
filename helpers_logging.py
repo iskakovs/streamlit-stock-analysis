95f86b5b728742d4a19892549523c426ef47462e
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-09 14:39:05 +0600

    Update helpers_logging.py

--- helpers_logging.py
+++ helpers_logging.py
@@ -4,3 +4,7 @@ import os #package is useful for joining information from different files (os.pa
 # define the log file and open logging session
 FILE_LOG = 'file.log'
 logger = logging.getLogger()
+
+# define init_logging function that uses 'dir_path' variable from 'run_streamlit' file. Everything to collect the logs
+def init_logging(dir_path, file_log='file.log', console_log_level='CRITICAL', file_log_level='CRITICAL',
+                 discard_old_info=False):
