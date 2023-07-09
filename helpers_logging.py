7df972536acd77a7158b2cab0049d2e62ec0a13b
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-09 14:38:27 +0600

    Update helpers_logging.py

--- helpers_logging.py
+++ helpers_logging.py
@@ -1,2 +1,6 @@
 import logging #package is responsible for collecting all the logs (see the file)
 import os #package is useful for joining information from different files (os.path.join)
+
+# define the log file and open logging session
+FILE_LOG = 'file.log'
+logger = logging.getLogger()
