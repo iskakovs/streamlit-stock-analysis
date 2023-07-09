e098a3e5ac39265f4c1b1e5dc8d12caa6076b4ee
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-09 14:40:02 +0600

    Update helpers_logging.py

--- helpers_logging.py
+++ helpers_logging.py
@@ -8,3 +8,24 @@ logger = logging.getLogger()
 # define init_logging function that uses 'dir_path' variable from 'run_streamlit' file. Everything to collect the logs
 def init_logging(dir_path, file_log='file.log', console_log_level='CRITICAL', file_log_level='CRITICAL',
                  discard_old_info=False):
+ """
+    create a console log and a file log
+
+    :param dir_path:
+    :type dir_path: str
+    :param file_log: log file name with extension log
+    :type file_log: str
+    :param console_log_level: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    :type console_log_level: str
+    :param file_log_level: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    :type file_log_level: str
+    :param discard_old_info:
+    :type discard_old_info: Boolean
+    :return: None
+    """
+    path = os.path.join(dir_path, file_log)
+
+    if discard_old_info:
+        with open(path, "w") as f:
+            f.write('')
+
