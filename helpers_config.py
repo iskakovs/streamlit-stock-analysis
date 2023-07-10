01490a496d718dffb6dea3250b439d8f51f49be5
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-10 12:42:09 +0600

    Update helpers_config.py

--- helpers_config.py
+++ helpers_config.py
@@ -2,3 +2,11 @@ import tomli #package 'tomli' is used to parse the 'config.toml' file
 
 # define function to open in binary mode (with the "rb" flag) and parse TOML
 def get_toml_data(path):
+    """
+    return config information as dict from a toml file
+
+    :param path: configuration file path file
+    :type path: str
+    :return: config information
+    :rtype: dict
+    """
