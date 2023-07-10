37041318f1749d18ac6d1e2f9800069da5080944
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-11 05:37:04 +0600

    Update db_sql.py

--- db_sql.py
+++ db_sql.py
@@ -5,3 +5,12 @@ import logging
 logger = logging.getLogger()
 
 def create_myTable(db_path, table_name='myTable'):
+    """
+    create a table if not exist
+    :param db_path:
+    :type db_path: string
+    :param table_name:
+    :type table_name: string
+    :return: None
+    """
+  
